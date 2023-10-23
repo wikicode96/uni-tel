fbb192d972a026f33c34f35649b8ee6372917111
CARLOS SOLER GARCIA <carlos.soler@viewnext.com>
2023-10-23 09:12:36 +0200

    admin microservice project

---
+++ admin/src/test/java/com/github/wikicode96/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wikicode96.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
